817316b71fa7950c687cdad4cdb09fc77613b15d
Daniel Soares <39413741+dansoares82@users.noreply.github.com>
2019-06-11 18:45:18 -0300

    Update index.php

--- projeto1-site-classificados/index.php
+++ projeto1-site-classificados/index.php
@@ -1,1 +1,19 @@
+<!--rodapé da topo-->
+<?php require 'pages/header.php'; ?>
 
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h1>Nós temos hoje mais de 60 anúncios</h1>
+			<h2>E mais de 10 usuários cadastrados</h2>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-3">
+				<h3>Pesquisa Avançada</h3>
+			</div>
+			<div class="col-sm-9">
+				<h3>Últimos anúncios</h3>
+			</div>
+		</div>
+<!--rodapé da pagina-->
+<?php require 'pages/footer.php'; ?>
